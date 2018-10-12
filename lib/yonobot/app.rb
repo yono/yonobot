cbd209c939d36e2609edceec351d7634ba59e89b
Yusaku ONO <yono.05@gmail.com>
2018-10-12 23:17:09 +0900

    extract to methods

--- lib/yonobot/app.rb
+++ lib/yonobot/app.rb
@@ -6,15 +6,12 @@ module Yonobot
     def tweet
       return if sleeping?
 
-      marcov = MarkovChain.new
-      tweet = Tweet.new
-      tweet.tweet(marcov.sentence)
+      tweet.tweet(sentence)
     end
 
     desc 'analysis', 'Analysis tweets.'
     def analysis
       csv = 'tweets.csv'
-      analyzer = Analyzer.new
       analyzer.store_csv(csv)
     end
 
@@ -22,14 +19,11 @@ module Yonobot
     def replies
       return if sleeping?
 
-      tweet = Tweet.new
       current = Time.now
-      marcov = MarkovChain.new
       tweet.mentions_timeline.each do |mention|
         # get only 10 minites
         next if mention.created_at + (60 * 10) < current
 
-        sentence = marcov.sentence
         tweet.reply("@#{mention.user.screen_name} #{sentence}", in_reply_to_status: mention)
       end
     end
@@ -38,6 +32,22 @@ module Yonobot
 
     def sleeping?
       Time.now.hour < 7
+    end
+
+    def marcov
+      @marcov ||= MarcovChain.new
+    end
+
+    def sentence
+      marcov.sentence
+    end
+
+    def tweet
+      @tweet ||= Tweet.new
+    end
+
+    def analyzer
+      @analyzer ||= Analyzer.new
     end
   end
 end
