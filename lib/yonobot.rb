dc0381caa4264220faeaa1a550dab79ccc48c31f
Yusaku ONO <yono.05@gmail.com>
2018-10-13 01:13:55 +0900

    fix requrie

--- lib/yonobot.rb
+++ lib/yonobot.rb
@@ -1,9 +1,9 @@
 module Yonobot
   require 'yonobot/version'
   require 'yonobot/app'
-  require 'yonobot/markovchain'
   require 'yonobot/tweet'
   require 'yonobot/mongodb'
-  require 'yonobot/analyzer'
   require 'yonobot/parser'
+  require 'yonobot/markovchain/analyzer'
+  require 'yonobot/markovchain/creator'
 end
