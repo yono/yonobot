b8f2ad5d9c5c00816608e454523068dd1e964923
Yusaku ONO <yono.05@gmail.com>
2018-10-13 01:12:55 +0900

    extract client to method

--- lib/yonobot/tweet.rb
+++ lib/yonobot/tweet.rb
@@ -4,25 +4,28 @@ Dotenv.load
 
 module Yonobot
   class Tweet
-    def initialize
-      @client = Twitter::REST::Client.new do |config|
+
+    def tweet(sentence)
+      client.update(sentence)
+    end
+
+    def reply(sentence, *args)
+      client.update(sentence, *args)
+    end
+
+    def mentions_timeline
+      client.mentions_timeline
+    end
+
+    private
+
+    def client
+      @client ||= Twitter::REST::Client.new do |config|
         config.consumer_key        = ENV['CONSUMER_KEY']
         config.consumer_secret     = ENV['CONSUMER_SECRET']
         config.access_token        = ENV['ACCESS_TOKEN']
         config.access_token_secret = ENV['ACCESS_TOKEN_SECRET']
       end
-    end
-
-    def tweet(sentence)
-      @client.update(sentence)
-    end
-
-    def reply(sentence, *args)
-      @client.update(sentence, *args)
-    end
-
-    def mentions_timeline
-      @client.mentions_timeline
     end
   end
 end
