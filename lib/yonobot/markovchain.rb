c9472b48dcb47560fef5ebeef5a73bdb1a9cf243
Yusaku ONO <yono.05@gmail.com>
2018-10-12 23:56:08 +0900

    Add comment

--- lib/yonobot/markovchain.rb
+++ lib/yonobot/markovchain.rb
@@ -12,11 +12,16 @@ module Yonobot
       prefix = START_SENTENCE
       loop do
         _suffix = collection.find_one(prefix: prefix)
+
+        # _suffix からランダムに1つ選ぶ
         suffix = _suffix["suffix"][rand(_suffix["suffix"].count)]
+
+        # 文末まで来たら終わり
         break if suffix == END_SENTENCE
 
         words << suffix
 
+        # prefixを1つずらす
         prefix.delete_at(0)
         prefix << suffix
       end
