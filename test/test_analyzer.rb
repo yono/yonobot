318941c41bbb6f4dae33ce6923a03aae25920f5e
Yusaku ONO <yono.05@gmail.com>
2018-10-14 21:45:29 +0900

    Add tests

--- test/test_analyzer.rb
+++ test/test_analyzer.rb
@@ -6,4 +6,28 @@ class TestAnalyzer < Test::Unit::TestCase
     result = analyzer.send(:remove_twitter_username, "@yono こんにちは")
     assert_equal result, " こんにちは"
   end
+
+  def test_remove_twitter_client_keywords
+    analyzer = ::Yonobot::MarkovChain::Analyzer.new
+    result = analyzer.send(:remove_twitter_client_keywords, "おはよう *tp *p *Tw*")
+    assert_equal result, "おはよう   "
+  end
+
+  def test_remove_twitter_hashtag
+    analyzer = ::Yonobot::MarkovChain::Analyzer.new
+    result = analyzer.send(:remove_twitter_hashtag, "こんばんは #ねむい")
+    assert_equal result, "こんばんは "
+  end
+
+  def test_store_csv
+    pend
+  end
+
+  def test_valid?
+    pend
+  end
+
+  def test_normalize
+    pend
+  end
 end
